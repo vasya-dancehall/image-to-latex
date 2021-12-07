2984df249145e8c07df73ed28f6d24459c55f9dd
Gleb Sokolov <gleb-sokolov@bk.ru>
2021-12-07 16:09:56 +0300

    commit

---
+++ scripts/artifacts.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import pandas as pd 
+import wandb
+
+api = wandb.Api()
+entity, project = "kingyiusuen", "image-to-latex"  # set to your entity and project 
+runs = api.runs(entity + "/" + project) 
+
+summary_list, config_list, name_list = [], [], []
+for run in runs: 
+	# .summary contains the output keys/values for metrics like accuracy.
+	#  We call ._json_dict to omit large files 
+	summary_list.append(run.summary._json_dict)
+	
+	# .config contains the hyperparameters.
+	#  We remove special values that start with _.
+	config_list.append(
+		{k: v for k,v in run.config.items()
+		if not k.startswith('_')})
+	
+	# .name is the human-readable name of the run.
+	name_list.append(run.name)
+	
+runs_df = pd.DataFrame({
+	"summary": summary_list,
+	"config": config_list,
+	"name": name_list
+	})
+
+print(runs_df)
