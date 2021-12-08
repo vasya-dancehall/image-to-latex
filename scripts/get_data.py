21d85b7cd64016b22afa6ad528596f9c7287c619
Gleb Sokolov <gleb-sokolov@bk.ru>
2021-12-08 16:22:04 +0300

    summary

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -2,9 +2,11 @@
 from google_drive_downloader import GoogleDriveDownloader as gdd
 
 #%%
-gdd.download_file_from_google_drive(file_id="1_IKXrpMhcl5DA1PfHWo1cWpNzCUJmrRi", dest_path="lst_files.tar", unzip=True)
+gdd.download_file_from_google_drive(
+    file_id="1_IKXrpMhcl5DA1PfHWo1cWpNzCUJmrRi", dest_path="/data/lst_files.tar", unzip=True
+)
 gdd.download_file_from_google_drive(
     file_id="1yLENoAWYa8BJV5Tomd0GCAYCwO0sESjc",
-    dest_path="formula_images_processed.tar",
+    dest_path="/data/formula_images_processed.tar",
     unzip=True,
 )
